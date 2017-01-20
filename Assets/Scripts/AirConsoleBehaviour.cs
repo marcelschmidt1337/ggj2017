f8a4926b8ba2ef04508257bcfb266556f5c1559f
Marcel Schmidt <marcelschmidt1337@gmail.com>
2017-01-21 00:23:39 +0100

    Added AirConsole

---
+++ Assets/Scripts/AirConsoleBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+public class AirConsoleBehaviour : MonoBehaviour
+{
+  private void Start ()
+  {
+    AirConsole.instance.onMessage += OnMessage;
+  }
+
+  private void OnMessage (int from, JToken data)
+  {
+    AirConsole.instance.Message (from, "Test");
+  }
+}
