db6973ca0a4a2edcba84cb9a27b93e921794a2a5
Anton Herzog <info@antonherzog.com>
2017-01-21 18:25:11 +0100

    Added test implementations.

---
+++ Assets/Scripts/TestRun/RowingView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowingView : MonoBehaviour {
+
+    public Animator Animator;
+    private const float AnimationLength = 1.292f;
+	// Use this for initialization
+	public void StartRowing () {
+        Animator.SetTrigger("StartRowing");
+	}
+	
+	// Update is called once per frame
+	public void StopRowing () {
+        Animator.SetTrigger("StopRowing");
+	}
+
+    public void SetAnimationDuration(float duration)
+    {
+        Animator.speed = duration / AnimationLength;
+    }
+}
---
+++ Assets/Scripts/TestRun/TestServer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestServer : NetworkManager
+{
+    private int worldClientId;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartServer();
+        NetworkServer.RegisterHandler(MsgType.Highest + 1, OnStartRowing);
+        NetworkServer.RegisterHandler(MsgType.Highest + 2, OnStopRowing);
+        NetworkServer.RegisterHandler((short)CustomMsgType.RegisterView, (netMsg) =>
+       {
+           worldClientId = netMsg.conn.connectionId;
+       });
+    }
+
+    private void OnStopRowing(NetworkMessage netMsg)
+    {
+    }
+
+    private void OnStartRowing(NetworkMessage netMsg)
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
