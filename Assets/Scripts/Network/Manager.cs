6379d01fe1cef8fe73c8eb2a7566769a4c2478b4
Marcel Schmidt <marcelschmidt1337@gmail.com>
2017-01-21 03:53:47 +0100

    Simple network messaging setup

---
+++ Assets/Scripts/Network/Manager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+public class Manager : NetworkManager
+{
+
+  private void OnGUI ()
+  {
+    if (GUILayout.Button ("Server"))
+    {
+      StartServer ();
+      NetworkServer.RegisterHandler (MsgType.Highest + 1, ReceiveCommand);
+
+    }
+    else if (GUILayout.Button ("Client"))
+    {
+      client = StartClient ();
+    }
+
+    if (client != null && GUILayout.Button ("SEND"))
+    {
+      client.Send (MsgType.Highest + 1, new StringMessage ("UNITY SUCKTT!"));
+    }
+  }
+
+  private void ReceiveCommand (NetworkMessage netMsg)
+  {
+    Debug.LogError (netMsg.ReadMessage<StringMessage> ().value);
+  }
+}
