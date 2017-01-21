cb64d11d0272eae4957558e253e1ff5783a68cd6
Anton Herzog <info@antonherzog.com>
2017-01-21 20:36:11 +0100

    Added missing using

--- Assets/Scripts/TestRun/TestView.cs
+++ Assets/Scripts/TestRun/TestView.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
 
 public class TestView : NetworkManager {
 
