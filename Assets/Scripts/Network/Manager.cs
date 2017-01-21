b310530173631b312a75adadfcd5516d57573f91
Friedrich Wessel <mail@friedrichwessel.de>
2017-01-21 04:04:35 +0100

    add Textfield

--- Assets/Scripts/Network/Manager.cs
+++ Assets/Scripts/Network/Manager.cs
@@ -5,27 +5,32 @@ using UnityEngine.Networking.NetworkSystem;
 public class Manager : NetworkManager
 {
 
+	private string received = "NOTHING"; 
   private void OnGUI ()
   {
-    if (GUILayout.Button ("Server"))
-    {
-      StartServer ();
-      NetworkServer.RegisterHandler (MsgType.Highest + 1, ReceiveCommand);
+	if (GUILayout.Button ("Server"))
+	{
+	  StartServer ();
+	  NetworkServer.RegisterHandler (MsgType.Highest + 1, ReceiveCommand);
 
-    }
-    else if (GUILayout.Button ("Client"))
-    {
-      client = StartClient ();
-    }
+	}
+	else if (GUILayout.Button ("Client"))
+	{
+	  client = StartClient ();
+	}
 
-    if (client != null && GUILayout.Button ("SEND"))
-    {
-      client.Send (MsgType.Highest + 1, new StringMessage ("UNITY SUCKTT!"));
-    }
+	if (client != null && GUILayout.Button ("SEND"))
+	{
+	  client.Send (MsgType.Highest + 1, new StringMessage ("UNITY SUCKTT!"));
+	}
+		GUILayout.Label(received); 
+
+
   }
 
   private void ReceiveCommand (NetworkMessage netMsg)
   {
-    Debug.LogError (netMsg.ReadMessage<StringMessage> ().value);
+		received = netMsg.ReadMessage<StringMessage>().value; 
+		Debug.LogError (received);
   }
 }
