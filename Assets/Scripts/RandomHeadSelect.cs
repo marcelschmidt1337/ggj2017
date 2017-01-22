ff0e01511755adaa400d0f297e6450c995b84130
Friedrich Wessel <mail@friedrichwessel.de>
2017-01-22 15:01:27 +0100

    Fixed Viewer Scenes

---
+++ Assets/Scripts/RandomHeadSelect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RandomHeadSelect : MonoBehaviour {
+
+	public static int currentHead = 0;
+
+	public void Start() {
+		SelectHat(currentHead);
+		currentHead++; 
+	}
+
+	public void SelectHat(int i)
+	{
+		i = Mathf.Clamp(i, 0, GetHatCount() - 1);
+		for (int j = 0; j < GetHatCount(); j++) {
+			transform.GetChild(j).gameObject.SetActive(j == i);
+		}
+	}
+
+	public int GetHatCount()
+	{
+		return transform.childCount;
+	}
+
+
+
+}
