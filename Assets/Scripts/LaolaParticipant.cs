cc5bc9bc2722069c6999dffe34813019e0ef6543
Friedrich Wessel <mail@friedrichwessel.de>
2017-01-22 05:05:47 +0100

    Test Intro Prefab

---
+++ Assets/Scripts/LaolaParticipant.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaolaParticipant : MonoBehaviour {
+
+	public Animator Animator;
+	public void StartLaola() {
+		this.Animator = gameObject.GetComponent<Animator>(); 
+		this.Animator.SetTrigger("StartRowing"); 
+	}
+
+	public void StopLaola() {
+		this.Animator = gameObject.GetComponent<Animator>();
+		this.Animator.SetTrigger("StopRowing");
+	}
+
+}
