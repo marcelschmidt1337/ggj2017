81a5f19636f21effa58eaf609e28581680038cf0
Anton Herzog <info@antonherzog.com>
2017-01-22 13:56:25 +0100

    Added particles for finished client row movement.

--- Assets/Scripts/TestRun/TestClient.cs
+++ Assets/Scripts/TestRun/TestClient.cs
@@ -10,6 +10,7 @@ public class TestClient : NetworkManager
 	public RowingView View;
 	public Slider Slider;
 	public UIManager UIManager;
+	public GameObject Yeah;
 	private float CountdownMove;
 	private float CountdownWait;
 	private float MoveDuration;
@@ -17,105 +18,111 @@ public class TestClient : NetworkManager
 
 	bool IsMoving = false;
 
-	public void JoinGroup (int group)
+	public void JoinGroup(int group)
 	{
 		if (client != null)
 		{
-			client.Send ((short)CustomMsgType.JoinGroup, new IntegerMessage (group));
+			client.Send((short)CustomMsgType.JoinGroup, new IntegerMessage(group));
 		}
 	}
 
-	public void LeaveGroup ()
+	public void LeaveGroup()
 	{
 		if (client != null)
 		{
-			client.Send ((short)CustomMsgType.LeaveGroup, new EmptyMessage ());
+			client.Send((short)CustomMsgType.LeaveGroup, new EmptyMessage());
 		}
 	}
 
-	public void JoinSide (int side)
+	public void JoinSide(int side)
 	{
 		if (client != null)
 		{
-			client.Send ((short)CustomMsgType.JoinSide, new IntegerMessage (side));
+			client.Send((short)CustomMsgType.JoinSide, new IntegerMessage(side));
 		}
 	}
 
-	public void LeaveSide ()
+	public void LeaveSide()
 	{
 		if (client != null)
 		{
-			client.Send ((short)CustomMsgType.LeaveSide, new EmptyMessage ());
+			client.Send((short)CustomMsgType.LeaveSide, new EmptyMessage());
 		}
 	}
 
-	public void ChangeHat (int hatIndex)
+	public void ChangeHat(int hatIndex)
 	{
 		if (client != null)
 		{
-			client.Send ((short)CustomMsgType.ChangeHat, new IntegerMessage (hatIndex));
-			UIManager.ShowWaiting (false);
+			client.Send((short)CustomMsgType.ChangeHat, new IntegerMessage(hatIndex));
+			UIManager.ShowWaiting(false);
 		}
 	}
 
-	public void StartClient (string ip)
+	public void StartClient(string ip)
 	{
 		if (client == null)
 		{
 			networkAddress = ip;
-			StartClient ();
-			client.RegisterHandler ((short)CustomMsgType.StartGame, StartGame);
+			StartClient();
+			client.RegisterHandler((short)CustomMsgType.StartGame, StartGame);
 		}
 
 		if (client != null)
 		{
-			Debug.Log ("Started client and connecting to " + ip);
+			Debug.Log("Started client and connecting to " + ip);
 		}
 		else
 		{
-			Debug.LogError ("Failed to start clien!");
+			Debug.LogError("Failed to start clien!");
 		}
 	}
 
-	private void StartGame (NetworkMessage netMsg)
+	private void StartGame(NetworkMessage netMsg)
 	{
-		Debug.Log ("CLIENT GAME STARTED");
-		UIManager.ShowClientUi ();
+		Debug.Log("CLIENT GAME STARTED");
+		UIManager.ShowClientUi();
 	}
 
-	public void ValueChanged (float value)
+	public void ValueChanged(float value)
 	{
 		if (client == null || !client.isConnected)
 		{
 			return;
 		}
 
+		if (value > .98)
+		{
+			Yeah.active = false;
+			Yeah.active = true;
+		}
+
 		if (!IsMoving)
 		{
-			client.Send ((short)CustomMsgType.StartRowing, new IntegerMessage (0));
+			client.Send((short)CustomMsgType.StartRowing, new IntegerMessage(0));
 			IsMoving = true;
 		}
 		else
 		{
 			if (value > 0.98f)
 			{
-				client.Send ((short)CustomMsgType.StopRowing, new IntegerMessage (0));
-				StartCoroutine (WaitForMoveReady ());
+				client.Send((short)CustomMsgType.StopRowing, new IntegerMessage(0));
+				StartCoroutine(WaitForMoveReady());
 			}
 		}
 	}
 
 	public void OnSliderRelease()
 	{
-		if(Slider.value > .98)
+		if (Slider.value > .98)
 		{
 			Slider.value = 0;
 		}
 	}
 
-	private IEnumerator WaitForMoveReady ()
+	private IEnumerator WaitForMoveReady()
 	{
-		yield return new WaitForSeconds (0.2f);
+		yield return new WaitForSeconds(0.2f);
 		IsMoving = false;
 	}
 }
