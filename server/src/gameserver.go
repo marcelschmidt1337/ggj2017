99b70e04a0e4bdd5e1b9a8c11b81d72ee739a981
Friedrich Wessel <mail@friedrichwessel.de>
2017-02-01 23:15:04 +0100

    Refactor send and receive in single functions

--- server/src/gameserver.go
+++ server/src/gameserver.go
@@ -3,6 +3,7 @@ package main
 import "net/http"
 import "fmt"
 import "github.com/gorilla/websocket"
+//import "encoding/json"
 
 var upgrader = websocket.Upgrader{
     CheckOrigin: func(r *http.Request) bool {
@@ -22,13 +23,23 @@ func wsPage(response http.ResponseWriter, request *http.Request){
         fmt.Println(err)
         return
     }
+    msg_type, msg := readIncommingCommand(conn)
+    sendCommand(conn, msg_type, msg)
+}
+
+func readIncommingCommand(conn *websocket.Conn) (msg_type int, msg []byte){
     msg_type, msg, read_err := conn.ReadMessage()
     if read_err != nil {
         fmt.Println(read_err)
     }
     fmt.Printf("Received Msg: " + string(msg[:]))
-    if read_err = conn.WriteMessage(msg_type, msg);
-    read_err != nil {
+    return msg_type, msg
+}
+
+func sendCommand(conn *websocket.Conn, msg_type int, msg []byte){
+    var write_err error = nil
+    if write_err = conn.WriteMessage(msg_type, msg)
+    write_err != nil {
         fmt.Println("Send error")
         return
     }
